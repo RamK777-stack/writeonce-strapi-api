efb30a375c2fd188fa91ad0099d87833f8ff4c5b
sowmiyabharani <sowmiyabharani7>
2022-02-02 08:55:47 +0530

    added url

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  // url: 'https://170f-42-109-140-231.ngrok.io',
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("URL", "https://api.writeonce.dev"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'ca526dbb2a2c73384499c4e4ac46eed4'),
+      secret: env("ADMIN_JWT_SECRET", "ca526dbb2a2c73384499c4e4ac46eed4"),
     },
   },
 });
